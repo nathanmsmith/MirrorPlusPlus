a367b8f4eb37b1d623d8321c3a7e800b36e28e4c
Nathan Smith <therealnathansmith@gmail.com>
2015-12-26 16:10:43 -0800

    Initial Commit

---
+++ Mirror/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mirror
+//
+//  Created by Nathan Smith on 12/26/15.
+//  Copyright © 2015 Smith Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
